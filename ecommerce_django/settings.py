3f8338bb19438ce177011baa8f3e9a802a11807c
Ahmed Mustafa <ahmed.mustafa@itworx.com>
2022-08-04 10:27:44 +0200

    update seetings II

--- ecommerce_django/settings.py
+++ ecommerce_django/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'website/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'website/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
