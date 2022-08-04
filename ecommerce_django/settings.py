674d97ed4ad9f86c02da19120404b92bd8557e54
Ahmed Mustafa <ahmed.mustafa@itworx.com>
2022-08-04 11:36:28 +0200

    updating configs

--- ecommerce_django/settings.py
+++ ecommerce_django/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,13 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # STATICFILES_DIR = [
-#     os.path.join(BASE_DIR, 'website/static')
+#     os.path.join(BASE_DIR, 'ecommerce_django/static/website')
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'website/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
