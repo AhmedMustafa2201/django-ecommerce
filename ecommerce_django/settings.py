db32588579d97160a140b8b625c8a10da725cee2
Ahmed Mustafa <ahmed.mustafa@itworx.com>
2022-08-04 10:05:16 +0200

    update settings for allowed hosts and debug

--- ecommerce_django/settings.py
+++ ecommerce_django/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=43buka2*+%la!0)_4)#sw#x&(bym0e72@d&3(u#79zz7pxqnf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-django-demo.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
